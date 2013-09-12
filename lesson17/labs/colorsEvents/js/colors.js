8567ffe781d48237f77e5e4291f482536e0e3a2c
Diana Jou <dianatjou@gmail.com>
2013-09-12 19:27:43 +0800

    getting json

---
+++ lesson17/labs/colorsEvents/js/colors.js
@@ -0,0 +1,28 @@
+
+
+$('#color_string').keypress(function(event){
+		
+
+
+});
+
+
+// $('html').mousemove(function(event){
+// 	// console.log(event.pageX);
+// 	// console.log(event.pageY);
+// 	$('#xLocation').text(event.pageX);
+// 	$('#yLocation').text(event.pageY);
+// });
+
+// $('#mouseBox').mousedown(function(hello){
+// 		function hello(){
+// 			console.log('hello world');
+
+// 		}
+// });
+
+// $('#mouseBox').on ('mousedown mouseup', function(event){
+		
+// 			console.log('hello world');
+	
+// });
